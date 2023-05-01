bda1385d1589da17ff8f338555b9f8c687725660
gmarvis <sgmarvis@gmail.com>
2023-05-01 07:10:05 +0100

    new feature changes in javascript

--- main.js
+++ main.js
@@ -42,23 +42,30 @@ form.addEventListener('submit', (e)=>{
             data.location=""
         }
 
+        // target the profile div in html to print the results data
+
+        // use avatar 
+
         document.getElementById('profile').innerHTML=`
         <a target="_blank" href="https://github.com/${userName}"> <img src="${data.avatar_url}"/> <a/>
         `
+
+        // print userName to the dom
         document.getElementById('name').innerText=`${data.name}`
 
-        document.getElementById('bio').innerText=`Bio : ${data.bio}`
+        // user bio
 
+        document.getElementById('bio').innerText=`Bio : ${data.bio}`
+        // followers and followings
         document.getElementById('following').innerText=`Followers : ${data.followers} | Following : ${data.following}`
 
+        // repositories
+
         document.getElementById('repo').innerText=`Repositories \n Public ${data.public_repos} | Private ${data.public_gists}`
+
+        // user location
         
         document.getElementById('location').innerText=`Location : ${data.location}`
-
-
-
-
-
     console.log(data)
     console.log(data.name)
     search = ''
@@ -68,10 +75,6 @@ form.addEventListener('submit', (e)=>{
     // .catch(error=>{
     //     alert(error)
     // })
-
-
-
-
 
 })
 
